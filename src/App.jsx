c7e37e8dc7f3201b5612ae667b38a986d553e9f1
Raghunandan <raghunandansharma615@gmail.com>
2024-01-11 14:33:47 +0530

    Change Directory Names and upgraded yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import V1Container from './components/V1Container';
-import Elements from './mfaComponents/Basics/Elements';
+import V1Container from './v1Components/V1Container';
+import Main from './Components/MainComponent/Main';
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path='/v1/*' element={<V1Container />} />
-				<Route path='/' element={<Elements />} />
+				<Route path='/*' element={<Main />} />
 			</Routes>	
 		</Router>
 	);
