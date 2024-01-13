7df2d455c68efb54bf788fc7176cb02c0db139b3
Raghunandan <raghunandansharma615@gmail.com>
2024-01-13 22:46:20 +0530

    Next task: creating animation in navbar

--- src/Components/NavbarComponent/Navbar.jsx
+++ src/Components/NavbarComponent/Navbar.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { faAddressCard, faCode, faGears, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard, faCode, faGears, faHome, faPlus, faUser } from '@fortawesome/free-solid-svg-icons'
 
 import Button from '../Basics/Button/Button'
 import styles from './navbar.module.scss'
+import { useMediaQuery } from 'react-responsive'
 
 const Navbar = () => {
+
+	const isDesktop = useMediaQuery({minWidth: 767})
 
   return (
 	<nav className={`${styles.navbar}`}>
 		<div className={`${styles.left}`}>
+			{isDesktop ? 
 			<Link to={`/`} className={`${styles.container}`}>
 				<Button name={'Home'} iconClass={faHome} />
-			</Link>
+			</Link> 
+			: <Button name={''} iconClass={faPlus} /> }
 		</div>
 		<div className={`${styles.right}`}>
+			{isDesktop ? null : 
+			<Link to={`/`} className={`${styles.container}`}>
+				<Button name={'Home'} iconClass={faHome} />
+			</Link>}
 			<Link to={`/about`} className={`${styles.container}`}>
 				<Button name={'About'} iconClass={faUser} />
 			</Link>
