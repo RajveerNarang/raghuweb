e266bca429d2b4ce57fc14db9d5d6b6aba06fea4
Raghunandan <raghunandansharma615@gmail.com>
2024-03-12 18:53:33 +0530

    Fixed: About Page animation fixed

--- src/Components/Basics/Dropdown/Dropdown.jsx
+++ src/Components/Basics/Dropdown/Dropdown.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import styles from '@components/Basics/Dropdown/dropdown.module.scss'
@@ -9,22 +9,41 @@ import Paragraph from '@components/Basics/Variables/Paragraph'
 const Dropdown = (props) => {
 	const {title, data, additionalClass, isOpen, toggleDropdown} = props
 
-  return (
-	<div className={`${styles.container} ${additionalClass || ''}`}>
-		<div className={`${styles.dropdownBtn}`} onClick={toggleDropdown}> {title} </div>
-		<div className={`${styles.dropdownContent} ${isOpen ? styles.slideDownAnimation : styles.slideUpAnimation}`}>
-			{data.content ? <Paragraph additionalClass={`${styles.marginTop}`} content={data.content}/> : null}
-			{data.lists ? <UnorderedList additionalClass={`${styles.marginTop}`} lists={data.lists}/> : null}
-			{data.path ? <File additionalClass={`${styles.marginTop}`} path={data.path} name={data.name} /> : null}
+	const [initialRender, setInitialRender] = useState(true);
+
+	const handleDropdownAnimation = () => {
+		toggleDropdown();
+
+		const dropdownContent = document.querySelector('.' + styles.dropdownContent);
+		const isExist = dropdownContent.classList.contains(styles.slideDownAnimation)
+		// console.log(isExist, initialRender);
+
+		if (isExist && initialRender) {
+			dropdownContent.classList.remove(styles.preload);
+			dropdownContent.classList.add(styles.slideUpAnimation);
+			setInitialRender(false)
+		}
+	};
+
+	return (
+		<div className={`${styles.container} ${additionalClass || ''}`}>
+			<div className={`${styles.dropdownBtn}`} onClick={handleDropdownAnimation}> {title} </div>
+			<div 
+				className={`${styles.dropdownContent} ${isOpen ? 
+					styles.slideDownAnimation : 
+					initialRender ? styles.preload : styles.slideUpAnimation
+				}`}
+			>
+				{data.content ? <Paragraph additionalClass={`${styles.marginTop}`} content={data.content}/> : null}
+				{data.lists ? <UnorderedList additionalClass={`${styles.marginTop}`} lists={data.lists}/> : null}
+				{data.path ? <File additionalClass={`${styles.marginTop}`} path={data.path} name={data.name} /> : null}
+			</div>
 		</div>
-		
-	</div>
-  )
+	)
 }
 
 Dropdown.prototype = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string
+  title: PropTypes.string.isRequired
 }
 
 export default Dropdown
