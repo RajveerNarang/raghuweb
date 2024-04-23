bb388265d5b10e17c45776a0682891db7bd97e04
Raghunandan <raghunandansharma615@gmail.com>
2024-04-23 17:20:39 +0530

    Updated App.jsx and deleted code of version 1 portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { lazy, Suspense } from 'react';
 
-// import V1Container from '@src/v1Components/V1Container';
-// import Main from '@components/MainComponent/Main';
 import Loader from '@components/Basics/Loader/Loader';
 
-const V1Container = lazy(() => import('@src/v1Components/V1Container'));
 const Main = lazy(() => import('@components/MainComponent/Main'));
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path='/v1/*' element={
-				<Suspense fallback={<Loader />}>
-					<V1Container />
-				</Suspense>} />
 				<Route path='/*' element={
 					<Suspense fallback={<Loader />}>
 						<Main />
