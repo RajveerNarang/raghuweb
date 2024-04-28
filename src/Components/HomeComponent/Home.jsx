33b49d766aed227e5049419b0384f48760ab71df
Raghunandan <raghunandansharma615@gmail.com>
2024-04-28 20:21:12 +0530

    Home Content completed

--- src/Components/HomeComponent/Home.jsx
+++ src/Components/HomeComponent/Home.jsx
@@ -2,21 +2,14 @@ import React from 'react'
 
 import styles from '@components/HomeComponent/home.module.scss'
 import GlassCard from '@components/Basics/GlassBox/GlassCard'
-import Header from '@components/Basics/Variables/Header'
-import Paragraph from '@components/Basics/Variables/Paragraph'
-import UnorderedList from '@components/Basics/Variables/UnorderedList'
-
-import homeData from '@json/frontend/home.json'
+import HomeContent from '@components/HomeComponent/HomeContent'
 
 const Home = () => {
 
   return (
 	<div className={`${styles.container}`}>
 		<GlassCard id={'homeGlassCard'} className={`${styles.box}`}>
-			<Header text={homeData.header} level={1} />
-			<Paragraph content={homeData.content} />
-			<UnorderedList lists={homeData.lists} className={styles.list_group}/>
-			<Paragraph content={homeData.content} />
+			<HomeContent />
 		</GlassCard>
   </div>
   )
---
+++ src/Components/HomeComponent/HomeContent.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Header from '@components/Basics/Variables/Header'
+import Paragraph from '@components/Basics/Variables/Paragraph'
+import UnorderedList from '@components/Basics/Variables/UnorderedList'
+import styles from '@components/HomeComponent/home.module.scss'
+import { useMediaQuery } from 'react-responsive'
+
+const HomeContent = () => {
+
+	const isDesktop = useMediaQuery({minWidth: 767})
+
+	const lists = [
+		{ text: 'Node.js'},
+		{ text: 'Express.js'},
+		{ text: 'React.js'},
+		{ text: 'Vertical Timeline Component'},
+		{ text: 'Framer Motion'},
+		{ text: 'Chart.js'},
+	]
+  return (
+	<>
+		<Header text={"Greetings Visitor!"} level={2} />
+		<Paragraph content="This platform is designed to share insights and experiences from my journey. It's a place where I express myself and showcase my work. From technical tutorials to personal reflections, you'll find a variety of topics covered here."/>
+
+		{isDesktop ? 
+		<>
+			<Header text={'Technology Utilized'} level={3} />
+			<Paragraph content="I applied various methodologies and techniques, such as adopting a mobile-first approach, emphasizing component reusability, and leveraging absolute paths. Additionally, I integrated cutting-edge technologies to enhance the functionality and performance of the platform." />
+			<UnorderedList className={styles.list_group} lists={lists}/>
+		</> : null}
+
+		<Header text="Notice:" level={4} />
+		<Paragraph content="Please note that the login feature is currently under development and won't enhance your experience at this time. However, stay tuned as I plan to add new features in the near future!" />
+		<Header text="Contact:" level={4} />
+		<Paragraph className={styles.spacing} content="For inquiries, feedback, or just to say hello, feel free to reach out! Your messages are greatly appreciated and will help shape the future of this platform." />
+	</>
+  )
+}
+
+export default HomeContent
