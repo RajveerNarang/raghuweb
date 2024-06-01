52d641bac630f670a5ab75073e622a84a3d79bf9
Raghunandan <raghunandansharma615@gmail.com>
2024-06-01 14:30:30 +0530

    Put some files to DUMP and updated packages

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ import { config } from 'dotenv';
 
 import express, { json } from 'express';
 
-// import { initialApp } from './fireBaseAdmin.js';
 import userRouter from './routes/userRouter.js';
-import audioRouter from './routes/audioRouter.js';
 import quoteRouter from './routes/quoteRouter.js';
 import messageRouter from './routes/messageRouter.js';
 import timelineRouter from './routes/timeLineRouter.js';
@@ -16,7 +14,6 @@ const server = express();
 server.use(json());
 
 server.use('/api/login', userRouter)
-server.use('/api/audio', audioRouter)
 server.use('/api/quote', quoteRouter)
 server.use('/api/message', messageRouter)
 server.use('/api/journey', timelineRouter)
