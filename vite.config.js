50a729932679acab296d1da71be1ed7f4c746461
RajveerNarang <dhanurrajveer@gmail.com>
2024-06-02 08:13:38 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,30 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({ 
+export default defineConfig({
   plugins: [
     react({
-      include: ['**/*.jsx', '**/*.js', '**/*.ts', '**/*.tsx'],
-    })
+      include: ["**/*.jsx", "**/*.js", "**/*.ts", "**/*.tsx"],
+    }),
   ],
+  base: "/raghuweb/",
   resolve: {
     alias: {
-      '@src': '/src',
-      '@components': '/src/components',
-      '@json': '/json',
-      '@images': '/images',
-      '@utils': '/utils',
-	  '@server': '/server',
+      "@src": "/src",
+      "@components": "/src/components",
+      "@json": "/json",
+      "@images": "/images",
+      "@utils": "/utils",
+      "@server": "/server",
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001', // Replace with your backend URL
+      "/api": {
+        target: "http://localhost:3001", // Replace with your backend URL
         changeOrigin: true,
-      }
-    }
-  }
+      },
+    },
+  },
 });
